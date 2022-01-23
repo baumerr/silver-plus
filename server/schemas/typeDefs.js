a0ac295e34c1018b15c37e127736697155d9a0ef
randy <baumer.randy@gmail.com>
2022-01-23 15:41:13 -0500

    added a typeDef for user-model

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,14 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
     type User {
+        _id: ID
         firstName: String
+        lastName: String
+        email: String
+        age: Number
+        location: String
+        athletic: Boolean
+        hobbies: [String]
     }
     
     type Query {
