945f53dcee56d92d1e33b3d55659ecdc92b162ae
fairfax <lovelace.af@gmail.com>
2022-02-01 00:07:19 -0500

    added skeleton for components, added skeleton for sign-up page, added page to begin work on jwt

---
+++ client/src/components/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section className='userProfile flex align-center'>
+            <p className='userText'>Insert profile information</p>
+        </section>
+    )
+}
