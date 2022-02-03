ce354bc71a986a74008cc89127059c7ecc5ca057
randy <baumer.randy@gmail.com>
2022-02-03 14:19:49 -0500

    added queries and mutations for front end

---
+++ client/src/utils/queries.js
@@ -0,0 +1,64 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_ME = gql`
+query me {
+    me {
+      _id
+      firstName
+      lastName
+      email
+      details {
+        _id
+        nickName
+        age
+        gender
+        hobbies
+        location
+        aboutMe
+        previousOccupation
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+query user ($_id: String!) {
+    user (_id: $_id) {
+      _id
+      firstName
+      lastName
+      email
+      details {
+        _id
+        nickName
+        age
+        previousOccupation
+        gender
+        hobbies
+        aboutMe
+        location
+      }
+    }
+  }
+`;
+
+export const QUERY_USERS = gql`
+query users {
+    users {
+      _id
+      firstName
+      lastName
+      email
+      details {
+        _id
+        nickName
+        age
+        previousOccupation
+        gender
+        hobbies
+        aboutMe
+        location
+      }
+    }
+  }
+`;
