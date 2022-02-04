2866245bf552cde86574e20bb91e969760641da0
shanthosh3 <y.shanthosh@gmail.com>
2022-02-04 18:02:45 -0500

    CSS update

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Auth from '../../utils/auth';
+import './Nav.css'
+import Logo from '../../assets/silver-plus-header.png'
 
 const Nav = () => {
     const logout = event => {
@@ -8,29 +10,29 @@ const Nav = () => {
         Auth.logout();
     };
     return (
-        <header className='bg-secondary mb-4 py-2 flex-row align-center'>
-            <div className='container flex-row justify-space-between-lg justify-center align-center'>
-                <Link to="/">
-                    <h1>Silver Plus: Dating for the Elderly</h1>
-                </Link>
+        <><div className='logo'>
+            <img src={Logo}></img>
+        </div><div className='container-main'>
+                <a href='/' class='Links'>
+                    <h1>Dating for the Elderly</h1>
+                </a>
 
-                <nav className='text-center'>
-                {Auth.loggedIn() ? (
-                <>
-                <Link to="/profile">My Silver Plus Profile</Link>
-                    <a href="/" onClick={logout}>
-                    Logout
-                    </a>
-                </>
-                ) : (
-                <>
-                    <Link to="/login">Login</Link>
-                    <Link to="/signup">Sign-Up</Link>
-                </>
-                )}
-                </nav>
-            </div>
-        </header>
+                <div className='text-center'>
+                    {Auth.loggedIn() ? (
+                        <>
+                            <a href="/profile" class='Links'>My Silver Plus Profile</a>
+                            <a href="/" onClick={logout}>
+                                Logout
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <a href="/login" class='Links'>Login</a> <br></br>
+                            <a href="/signup" class='Links'>Sign-Up</a>
+                        </>
+                    )}
+                </div>
+            </div></>
     );
 };
 
